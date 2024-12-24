46ebf966c1e93716887e63e7fb47a76d4bb5847f
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-12-24 16:31:35 +0600

    update and added not found route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './app/router/router';
 import { globalMiddleWare } from './app/middleware/globalMiddleware';
+import notFound from './app/middleware/noFound';
 
 const app = express();
 app.use(express.json());
 app.use('/api', router);
+app.use(notFound)
 app.use(globalMiddleWare);
 
 export default app;
